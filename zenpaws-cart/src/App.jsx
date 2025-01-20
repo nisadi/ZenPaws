8aa4ad29597228061d90a33c42146fb6cba923ba
Shanuka Wanniarachchi <shanukawanniarachchi2000@gmail.com>
2025-01-20 20:26:49 +0530

    home-page-ubdate

--- zenpaws-cart/src/App.jsx
+++ zenpaws-cart/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Cart from './Components/Cart';
 import './App.css'
 
 function App() {
@@ -8,6 +7,7 @@ function App() {
 
   return (
     <>
+    <Cart/>
     </>
   )
 }
