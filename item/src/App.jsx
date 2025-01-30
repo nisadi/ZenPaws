aedaa9872b0028e1c34588cfd443178599f60900
pasindumathsara <pasinduuni23@gmail.com>
2025-01-30 22:56:53 +0530

    item

---
+++ item/src/App.jsx
@@ -0,0 +1,14 @@
+import ProductPage from './components/ProductPage';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductPage />
+      
+    </div>
+  );
+}
+
+export default App;
+
+  
